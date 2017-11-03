eb7c6256ef12ee276d1ce9f8c26318f6af96c59f
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-03 01:54:41 -0400

    Remove example files.

--- hello_world_server.c
+++
@@ -1,6 +0,0 @@
-#include "msg.h"
-
-void* print_name_1_svc(char** txt, struct svc_req *req)
-{
-  printf("Hello world: %s\n", *txt);
-}
--- msg.x
+++
@@ -1,6 +0,0 @@
-program HELLOWORLD {
-  version HELLO_V1 {
-    void PRINT_NAME(string) = 1;
-  } = 1;
-} = 0x2ffffff;
-
