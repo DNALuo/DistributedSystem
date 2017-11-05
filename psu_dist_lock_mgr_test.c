fdd41e02b54a52c17f1199bb462b29abf28dbd12
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-05 13:45:39 -0500

    Add a simple test.

--- psu_dist_lock_mgr_test.c
+++ psu_dist_lock_mgr_test.c
@@ -1,7 +1,11 @@
+#include <unistd.h>
 #include "psu_dist_lock_mgr.h"
 
 int main(int argc, char **argv)
 {
   char *nodes[] = {"localhost", "localhost", "localhost"};
   psu_init_lock_mgr(nodes, sizeof(nodes)/sizeof(nodes[0]));
+  psu_acquire_lock(1);
+  sleep(1);
+  psu_release_lock(1);
 }
