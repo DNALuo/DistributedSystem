0322229ec44c94fb0e1150565ccf17ed36b2c347
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-07 23:43:33 -0500

    Fix the num_nodes argument.

--- psu_dist_lock_mgr_test.c
+++ psu_dist_lock_mgr_test.c
@@ -3,10 +3,8 @@
 
 int main(int argc, char **argv)
 {
-  char *nodes[1024];
-  for(int i = 1; i < argc; ++i)
-    nodes[i - 1] = argv[i];
-  psu_init_lock_mgr(nodes, sizeof(nodes)/sizeof(nodes[0]));
+  char **nodes = &argv[1];
+  psu_init_lock_mgr(nodes, argc - 1);
   psu_acquire_lock(1);
   sleep(1);
   psu_release_lock(1);
