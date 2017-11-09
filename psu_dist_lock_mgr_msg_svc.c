8c13c6360ed0a71d3fb58312b4d930b5aba447d6
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-09 14:39:22 -0500

    Remove redundent includes.

--- psu_dist_lock_mgr_msg_svc.c
+++ psu_dist_lock_mgr_msg_svc.c
@@ -2,10 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <rpc/pmap_clnt.h>
-#include <string.h>
 #include <memory.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
 #ifndef SIG_PF
 #define SIG_PF void(*)(int)
