51e0e08f309d66e0612efdb09c5d10847faca090
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-12 23:45:21 -0500

    Move argument parsing forward.

--- psu_dist_lock_mgr_msg_svc.c
+++ psu_dist_lock_mgr_msg_svc.c
@@ -12,68 +12,84 @@
 const char *procedures[] = {"NULLPROC", "INIT_LOCK_MGR", "ACQUIRE_LOCK", "RELEASE_LOCK", "REQUEST"};
 extern void initialize_global_variable();
 
+struct thread_data
+{
+  struct svc_req *rqstp;
+  SVCXPRT *transp;
+  caddr_t argument;
+  xdrproc_t xdr_argument, xdr_result;
+  bool_t (*local)(char *, void *, struct svc_req *);
+};
+
+static void parse_thread_data(struct svc_req *rqstp, register SVCXPRT *transp, struct thread_data *thread_data)
+{
+  union argument_union{
+    char *init_lock_mgr_1_arg;
+    int acquire_lock_1_arg;
+    int release_lock_1_arg;
+    RequestPack request_1_arg;
+  } *argument = (union argument_union *)malloc(sizeof(union argument_union));
+
+  xdrproc_t _xdr_argument = NULL, _xdr_result = NULL;
+  bool_t (*local)(char *, void *, struct svc_req *) = NULL;
+  switch (rqstp->rq_proc) {
+    case INIT_LOCK_MGR:
+      _xdr_argument = (xdrproc_t) xdr_wrapstring;
+      _xdr_result = (xdrproc_t) xdr_void;
+      local = (bool_t (*) (char *, void *,  struct svc_req *))init_lock_mgr_1_svc;
+      break;
+
+    case ACQUIRE_LOCK:
+      _xdr_argument = (xdrproc_t) xdr_int;
+      _xdr_result = (xdrproc_t) xdr_void;
+      local = (bool_t (*) (char *, void *,  struct svc_req *))acquire_lock_1_svc;
+      break;
+
+    case RELEASE_LOCK:
+      _xdr_argument = (xdrproc_t) xdr_int;
+      _xdr_result = (xdrproc_t) xdr_void;
+      local = (bool_t (*) (char *, void *,  struct svc_req *))release_lock_1_svc;
+      break;
+
+    case REQUEST:
+      _xdr_argument = (xdrproc_t) xdr_RequestPack;
+      _xdr_result = (xdrproc_t) xdr_void;
+      local = (bool_t (*) (char *, void *,  struct svc_req *))request_1_svc;
+      break;
+
+    default:
+      printf("No procedure Found!\n");
+      svcerr_noproc (transp);
+  }
+  memset ((char *)argument, 0, sizeof (*argument));
+  if (!svc_getargs (transp, (xdrproc_t) _xdr_argument, (caddr_t) argument)) {
+    printf("Error getting arguments.\n");
+    svcerr_decode (transp);
+  }
+
+  thread_data->local = local;
+  thread_data->rqstp = rqstp;
+  thread_data->transp = transp;
+  thread_data->argument = (caddr_t) argument;
+  thread_data->xdr_result = _xdr_result;
+  thread_data->xdr_argument = _xdr_argument;
+}
+
 static void *psu_dist_lock_mgr_1_run(void *data)
 {
   // unpack the thread data
-  struct thread_data
-  {
-    struct svc_req *rqstp;
-    SVCXPRT *transp;
-  } *ptr_data;
+  struct thread_data *ptr_data = (struct thread_data *)data;
+  struct svc_req *rqstp = ptr_data->rqstp;
+  SVCXPRT *transp = ptr_data->transp;
+  caddr_t argument = ptr_data->argument;
+  xdrproc_t _xdr_argument = ptr_data->xdr_argument, _xdr_result = ptr_data->xdr_result;
+  bool_t (*local)(char *, void *, struct svc_req *) = ptr_data->local;
 
-  ptr_data = (struct thread_data  *)data;
-  struct svc_req *rqstp = ptr_data-> rqstp;
-  register SVCXPRT *transp = ptr_data-> transp;
+  bool_t retval;
+  union {
+  } result;
 
-	union {
-		char *init_lock_mgr_1_arg;
-		int acquire_lock_1_arg;
-		int release_lock_1_arg;
-		RequestPack request_1_arg;
-	} argument;
-	union {
-	} result;
-	bool_t retval;
-	xdrproc_t _xdr_argument, _xdr_result;
-	bool_t (*local)(char *, void *, struct svc_req *);
-
-	switch (rqstp->rq_proc) {
-	case INIT_LOCK_MGR:
-		_xdr_argument = (xdrproc_t) xdr_wrapstring;
-		_xdr_result = (xdrproc_t) xdr_void;
-		local = (bool_t (*) (char *, void *,  struct svc_req *))init_lock_mgr_1_svc;
-		break;
-
-	case ACQUIRE_LOCK:
-		_xdr_argument = (xdrproc_t) xdr_int;
-		_xdr_result = (xdrproc_t) xdr_void;
-		local = (bool_t (*) (char *, void *,  struct svc_req *))acquire_lock_1_svc;
-		break;
-
-	case RELEASE_LOCK:
-		_xdr_argument = (xdrproc_t) xdr_int;
-		_xdr_result = (xdrproc_t) xdr_void;
-		local = (bool_t (*) (char *, void *,  struct svc_req *))release_lock_1_svc;
-		break;
-
-	case REQUEST:
-		_xdr_argument = (xdrproc_t) xdr_RequestPack;
-		_xdr_result = (xdrproc_t) xdr_void;
-		local = (bool_t (*) (char *, void *,  struct svc_req *))request_1_svc;
-		break;
-
-	default:
-    printf("No procedure Found!\n");
-		svcerr_noproc (transp);
-		return NULL;
-	}
-	memset ((char *)&argument, 0, sizeof (argument));
-	if (!svc_getargs (transp, (xdrproc_t) _xdr_argument, (caddr_t) &argument)) {
-    printf("Error getting arguments.\n");
-		svcerr_decode (transp);
-		return NULL;
-	}
-	retval = (bool_t) (*local)((char *)&argument, (void *)&result, rqstp);
+	retval = (bool_t) (*local)((char *)argument, (void *)&result, rqstp);
 	if (retval > 0 && !svc_sendreply(transp, (xdrproc_t) _xdr_result, (char *)&result)) {
 		svcerr_systemerr (transp);
 	}
@@ -96,25 +112,16 @@ static void psu_dist_lock_mgr_1(struct svc_req *rqstp, register SVCXPRT *transp)
     return;
   }
 
-  struct thread_data
-  {
-    struct svc_req *rqstp;
-    SVCXPRT *transp;
-  } *data_ptr=(struct thread_data *)malloc(sizeof(struct thread_data));
+  struct thread_data *data_ptr=(struct thread_data *)malloc(sizeof(struct thread_data));
 
-  // deep copy the rqstp and transp pointers
-  struct svc_req *rqstp_copy = (struct svc_req *)malloc(sizeof(struct svc_req));
-  memcpy(rqstp_copy, rqstp, sizeof(struct svc_req));
-  SVCXPRT *transp_copy = (SVCXPRT *)malloc(sizeof(SVCXPRT));
-  memcpy(transp_copy, transp, sizeof(SVCXPRT));
+  parse_thread_data(rqstp, transp, data_ptr);
 
   printf("\033[32;1mProcedure calls %s\033[0m\n", procedures[rqstp->rq_proc]);
 
-  data_ptr->rqstp = rqstp_copy;
-  data_ptr->transp = transp_copy;
   pthread_t *thread= (pthread_t *)malloc(sizeof(pthread_t));
   pthread_create(thread, NULL, psu_dist_lock_mgr_1_run, (void *)data_ptr);
   pthread_detach(*thread);
+
 }
 
 int main (int argc, char **argv)
