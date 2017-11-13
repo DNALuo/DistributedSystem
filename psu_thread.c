114565bf677d41b00d8698059616d00081471d88
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-13 16:49:20 -0500

    Move struct definition to header file.

--- psu_thread.c
+++ psu_thread.c
@@ -2,12 +2,6 @@
 #include <ucontext.h>
 #include <signal.h>
 
-
-typedef struct psu_thread_info psu_thread_info_t
-{
-
-};
-
 int psu_thread_create(psu_thread_info_t* t_info, void* (*start_routine)(void*), void* args)
 {
   ucontext_t a;
