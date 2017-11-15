125b760c016ad241c223301d7d603b07d6db8fff
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-15 12:59:39 -0500

    __sigset_t is a typedef not struct.

--- psu_thread_server.c
+++ psu_thread_server.c
@@ -29,7 +29,7 @@ bool_t migrate_1_svc(rpc_ucontext *context, void *res, struct svc_req *req)
   memcpy(cont.uc_mcontext.fpregs, &context->uc_mcontext.fpregs, sizeof(struct _libc_fpstate));
   memcpy(cont.uc_mcontext.gregs, context->uc_mcontext.gregs, sizeof(gregset_t));
 
-  memcpy(&cont.uc_sigmask, &context->uc_sigmask, sizeof(struct __sigset_t));
+  memcpy(&cont.uc_sigmask, &context->uc_sigmask, sizeof(__sigset_t));
 
   memcpy(&cont.__fpregs_mem, &context->__fpregs_mem, sizeof(struct _libc_fpstate));
 
