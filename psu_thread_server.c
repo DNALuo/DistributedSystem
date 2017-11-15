b3a9d0d9742299a0b30d3977565b5f44e3acd5a5
Ryan Wang <ryanwang.cs@gmail.com>
2017-11-15 12:46:51 -0500

    Implement server procedure.

--- psu_thread_server.c
+++ psu_thread_server.c
@@ -7,10 +7,33 @@
 #include <unistd.h>
 #include "utility.h"
 #include "psu_thread_msg.h"
+#define _XOPEN_SOURCE
+#include <ucontext.h>
 
 bool_t migrate_1_svc(rpc_ucontext *context, void *res, struct svc_req *req)
 {
   printf("Server received.\n");
+  ucontext_t cont;
+
+  cont.uc_flags = context->uc_flags;
+
+  // ucontext.stack_t
+  cont.uc_stack.ss_flag = context->uc_stack.ss_flags;
+  cont.uc_stack.ss_sp = context->uc_stack.ss_sp.ss_sp_val;
+  cont.uc_stack.ss_size = context->uc_stack.ss_sp.ss_sp_len;
+
+  // ucontext.mcontext_t
+  memcpy(cont.uc_mcontext.__reserved1, context->uc_mcontext.__reserved1, sizeof(cont.uc_mcontext.__reserved1));
+  cont.uc_mcontext.fpregs = (struct _libc_fpstate *)malloc(sizeof(struct _libc_fpstate));
+  memcpy(cont.uc_mcontext.fpregs, &context->uc_mcontext.fpregs, sizeof(struct _libc_fpstate));
+  memcpy(cont.uc_mcontext.gregs, context->uc_mcontext.gregs, sizeof(gregset_t));
+
+  memcpy(&cont.uc_sigmask, &context->uc_sigmask, sizeof(struct __sigset_t));
+
+  memcpy(&cont.__fpregs_mem, &context->__fpregs_mem, sizeof(struct _libc_fpstate));
+
+  printf("Setting the context.\n");
+  setcontext(&cont);
   return true;
 }
 
